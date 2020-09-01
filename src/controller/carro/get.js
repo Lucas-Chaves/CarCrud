9c0f08ff1c96d9a73c067a0fc12507b7e076a480
Lucas Chaves <lucas.chaves@outlook.com>
2020-09-01 18:40:27 -0300

    add search by query name

--- src/controller/carro/get.js
+++ src/controller/carro/get.js
@@ -2,7 +2,9 @@ const query = require('../../../knex/knex')
 
 const getCar = async (req, res) => {
     id = req.params.id
+    name = req.query.name
     if (id != null) getbyId(req, res)
+    if (name != null) getbyQuery(req,res)
     else getAllCars(req, res)
 }
 
@@ -29,6 +31,16 @@ const getbyId = async (req, res) => {
         return res.status(400).send(e)
     }
 
+}
+
+const getbyQuery = async (req, res) => {
+    try {
+        const carros = await query.select().where('veiculo', req.query.name).from('carros')
+        return res.status(200).json(carros)
+    } catch (e) {
+        e.message = "Missing some information"
+        return res.status(400).send(e)
+    }
 }
 
 
